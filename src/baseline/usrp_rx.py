8b8c13e81913d449db2e23132ac35cfeed6dedfc
Quentin <quentin.prieels@gmail.com>
2024-10-21 17:33:32 +0200

    Python formatting

--- src/baseline/usrp_rx.py
+++ src/baseline/usrp_rx.py
@@ -16,33 +16,45 @@ Code from:
 """
 # The constellations
 from radcomlib.comm_toolbox import BPSK_const, QPSK_const, QAM16_const
+
 # Inverse mapping from symbols to bits
 from radcomlib.comm_toolbox import inverse_mapping
+
 # OFDM RX Radar processing
 from radcomlib.ofdm import SISO_OFDM_DFRC_RADAR_RX
+
 # OFDM Rx Communication processing
-from radcomlib.ofdm import OFDM_synchronisation,OFDM_demodulation,OFDM_channel_estimate,OFDM_channel_equalisation, OFDM_modulation
+from radcomlib.ofdm import (
+    OFDM_synchronisation,
+    OFDM_demodulation,
+    OFDM_channel_estimate,
+    OFDM_channel_equalisation,
+    OFDM_modulation,
+)
+
 # Correlation function for the radar processing
 from radcomlib.radar_toolbox import pulse_correlation
 
 # The usual imports
-#from scipy.signal import correlate as scipy_correlate
+# from scipy.signal import correlate as scipy_correlate
 import matplotlib.pyplot as plt
 import numpy as np
 
 """
 #1. Subfunction definition
 """
+
+
 def process_rx(filename):
     """
-    The function opens the <.txt> file from the reception with the 
+    The function opens the <.txt> file from the reception with the
     experimental setup. The file must be moved from RamDisk to the current
-    directory as defined in the jupyter notebook "section 4: Reception d'un 
+    directory as defined in the jupyter notebook "section 4: Reception d'un
     signal"
-    
+
     Parameters
     ----------
-    
+
     filename : string <filename.txt>
         name of the file with the I/Q samples received
 
@@ -52,19 +64,20 @@ def process_rx(filename):
         vector with the I/Q samples received
 
     """
-    with open(filename, 'rb') as file:
+    with open(filename, "rb") as file:
         data = np.fromfile(file, dtype=np.float32)
         data = data.astype(np.complex64)
 
-        rx_sig = data[::2] + 1j*data[1::2]
-        rx_sig.reshape(-1,1)
+        rx_sig = data[::2] + 1j * data[1::2]
+        rx_sig.reshape(-1, 1)
         rx_sig = np.squeeze(rx_sig)
 
     return rx_sig
 
+
 """
 #2. PARAMETERS for the OFDM transmission
-""" 
+"""
 ###############################################################################
 # 2.1 These parameters were defined at the transmitter
 ###############################################################################
@@ -73,52 +86,51 @@ def process_rx(filename):
 scenario_name = "Setup_40MHz"
 
 # Load the parameters for the given scenario
-MyDic_param = np.load(scenario_name+"_"+"parameters.npy", allow_pickle=True).item()
-# Number of transmitted pulses (number of OFDM symbols) 
-P = MyDic_param['P'] #default: same as Tx file
+MyDic_param = np.load(scenario_name + "_" + "parameters.npy", allow_pickle=True).item()
+# Number of transmitted pulses (number of OFDM symbols)
+P = MyDic_param["P"]  # default: same as Tx file
 # Bandwidth [Hz]: The maximum available bandwith is 100 MHz
-B = MyDic_param['B'] #default: same as Tx file
+B = MyDic_param["B"]  # default: same as Tx file
 # Number of subcarriers
-N_sc = MyDic_param['N_sc'] #default: same as Tx file
-# Cyclic Prefix (CP) length 
-L_CP = MyDic_param['L_CP'] #default: same as Tx file
-# Rx oversampling factor: 
-M = MyDic_param['L'] #default: same as Tx file
+N_sc = MyDic_param["N_sc"]  # default: same as Tx file
+# Cyclic Prefix (CP) length
+L_CP = MyDic_param["L_CP"]  # default: same as Tx file
+# Rx oversampling factor:
+M = MyDic_param["L"]  # default: same as Tx file
 # Speed of light
-c = 299792458 
-
+c = 299792458
 
 ###############################################################################
 # 2.2 Checks for potential errors
 ###############################################################################
 
-# The maximum sampling rate of the setup is 200MHz for the transmission. 
-# Therefore the product of the bandwith and the oversampling factor must be 
+# The maximum sampling rate of the setup is 200MHz for the transmission.
+# Therefore the product of the bandwith and the oversampling factor must be
 # smaller or equal to 200 MHz
-assert(M*B<=200e6)
+assert M * B <= 200e6
 # The CP length must be smaller than the number of subcarriers
-assert(L_CP<N_sc)
+assert L_CP < N_sc
 
 ###############################################################################
-# 2.3 These values are directly defined from the above parameters 
+# 2.3 These values are directly defined from the above parameters
 ###############################################################################
 
 # Subcarrier spacing [Hz]
-Df = B/N_sc 
+Df = B / N_sc
 # OFDM symbol duration [s]
-T = 1/Df 
-# Time between two samples in time domain if no oversampling factor [s] 
-Tc = 1/B
+T = 1 / Df
+# Time between two samples in time domain if no oversampling factor [s]
+Tc = 1 / B
 # Rx sampling frequency [Hz]
-fs_rx = M/Tc 
+fs_rx = M / Tc
 # Duration of the CP [s]
-T_CP = L_CP*Tc
+T_CP = L_CP * Tc
 # Duration of the full OFDM symbol (with the CP included) [s]
 T_PRI = T + T_CP
 # Number of samples in the useful part of the OFDM symbols
-pulse_width = N_sc*M 
+pulse_width = N_sc * M
 # Number of samples in the CP part of the OFDM symbols
-Ng = L_CP*M 
+Ng = L_CP * M
 
 """
 #3. Parameters for the pilots and data symbols
@@ -128,15 +140,15 @@ Ng = L_CP*M
 ###############################################################################
 
 # Constellation ; either "BPSK", "QPSK", "16QAM"
-const_type = MyDic_param['const_type'] # Default: same as Tx file
-# Pilot spacing in the time domain (defines how often the OFDM symbol will 
+const_type = MyDic_param["const_type"]  # Default: same as Tx file
+# Pilot spacing in the time domain (defines how often the OFDM symbol will
 # countain pilots)
-Nt = MyDic_param['Nt'] # Default: same as Tx file
-# Pilot spacing in the frequency domain (defines the spacing in number of 
+Nt = MyDic_param["Nt"]  # Default: same as Tx file
+# Pilot spacing in the frequency domain (defines the spacing in number of
 # subcarriers  between two pilots subcarriers)
-Nf = MyDic_param['Nf'] # Default: same as Tx file
+Nf = MyDic_param["Nf"]  # Default: same as Tx file
 # For example: P=4, N_sc = 8, Nt = 1 and Nf = 2:
-# The subcarriers 0,2,4,6,7 will have pilots for all OFDM symbols 0,1,2,3. All 
+# The subcarriers 0,2,4,6,7 will have pilots for all OFDM symbols 0,1,2,3. All
 # the other subcarriers from the OFDM symbols are data symbols. Note that
 # subcarrier 7 is included to enable interpolation and avoid extrapolation.
 
@@ -149,14 +161,14 @@ Nf = MyDic_param['Nf'] # Default: same as Tx file
 # If Zero padding is set to 1, then no zero padding is applied. If we set it to
 # 2, then we add one point between each grid point
 # Zero padding for the range
-zeropad_N = 1 # Default: zeropad_N = 1
+zeropad_N = 1  # Default: zeropad_N = 1
 # Zero padding for the doppler
-zeropad_P = 1 # Default: zeropad_P = 1
+zeropad_P = 1  # Default: zeropad_P = 1
 
 
 ## RADAR grid spacing
-dtau = 1/B / M / zeropad_N # delay
-df_D = 1/(P*T_PRI) / zeropad_P # Doppler frequency
+dtau = 1 / B / M / zeropad_N  # delay
+df_D = 1 / (P * T_PRI) / zeropad_P  # Doppler frequency
 
 
 """
@@ -174,30 +186,30 @@ elif const_type == "16QAM":
 
 ## Generation of the pilots grid
 
-# Note: the first and the last subcarrier is always included in the 
-# pilots. Same for the first and the last OFDM symbol. This ensure that we will 
+# Note: the first and the last subcarrier is always included in the
+# pilots. Same for the first and the last OFDM symbol. This ensure that we will
 # not perform any extrapolation.
-pilots_idx_f = np.concatenate((np.arange(0,N_sc-1,Nf),[N_sc-1])) 
-pilots_idx_t = np.concatenate((np.arange(0,P-1,Nt),[P-1]))
+pilots_idx_f = np.concatenate((np.arange(0, N_sc - 1, Nf), [N_sc - 1]))
+pilots_idx_t = np.concatenate((np.arange(0, P - 1, Nt), [P - 1]))
 # Representation in two dimensions of the pilots in frequency and time domain
-pilots_idx_t_mesh,pilots_idx_f_mesh = np.meshgrid(pilots_idx_t,pilots_idx_f)
+pilots_idx_t_mesh, pilots_idx_f_mesh = np.meshgrid(pilots_idx_t, pilots_idx_f)
 
-## Load the symbols generated for data and preamble 
+## Load the symbols generated for data and preamble
 
-# Matrix of size: Number of OFDM symbols x Number of subcarriers (countains 
+# Matrix of size: Number of OFDM symbols x Number of subcarriers (countains
 # both the pilots and data symbols)
-symbols = np.load(scenario_name+"_"+"symbols.npy")
+symbols = np.load(scenario_name + "_" + "symbols.npy")
 # Vector of size: Number of subcarriers (countains only the preamble symbols
-# transmitted for the packet detection). 
-preamble = np.load(scenario_name+"_"+"preamble.npy")
+# transmitted for the packet detection).
+preamble = np.load(scenario_name + "_" + "preamble.npy")
 
 ## Get the corresponding pilots and bits
 
 # Pilots symbols matrix of size: sizeof(pilots_idx_t) x sizeof(pilots_idx_f)
-pilots = symbols[pilots_idx_t_mesh.T,pilots_idx_f_mesh.T] 
-# Corresponding bits for the data and pilot symbol of size: 
+pilots = symbols[pilots_idx_t_mesh.T, pilots_idx_f_mesh.T]
+# Corresponding bits for the data and pilot symbol of size:
 # Number of OFDM symbols*Number of subcarriers
-bits = inverse_mapping(symbols.reshape((P*N_sc,)), const_type)
+bits = inverse_mapping(symbols.reshape((P * N_sc,)), const_type)
 
 ## Load the received signal
 rx_sig = process_rx("rx_sig.dat")
@@ -207,41 +219,48 @@ rx_sig = process_rx("rx_sig.dat")
 """
 
 ###############################################################################
-# 5.1 Synchronize the signal by correlating with the preamble 
+# 5.1 Synchronize the signal by correlating with the preamble
 ###############################################################################
 
-# Get the maximum of the correlation in the first half of the signal to 
+# Get the maximum of the correlation in the first half of the signal to
 # ensure that we have a full symbol afterwards
-_,max_idx = pulse_correlation(rx_sig[:len(rx_sig)//2]) 
+preamble_pulse = OFDM_modulation(np.array([preamble]), L_CP, M)
+pulse_corr, max_idx = pulse_correlation(rx_sig[: len(rx_sig) // 2], preamble_pulse)
 # Move the begin of the vector to the beginning of the preamble
 r_sync = rx_sig[max_idx:]
+
+plt.figure()
+plt.plot(np.abs(pulse_corr))
+plt.plot([max_idx, max_idx], [0, np.max(np.abs(pulse_corr))])
+plt.show()
+
 
 ###############################################################################
 # 5.2 Demodulate the OFDM signal
 ###############################################################################
 
 # We also demodulates the preamble hence the P+1
-y_with_preamble = OFDM_demodulation(r_sync, P+1, N_sc, L_CP, M)
+y_with_preamble = OFDM_demodulation(r_sync, P + 1, N_sc, L_CP, M)
 # Remove the first OFDM symbol since its the preamble
-y = y_with_preamble[1:,:] 
+y = y_with_preamble[1:, :]
 
 ###############################################################################
 # 5.3 Perform the OFDM channel estimation
 ###############################################################################
 
-channel_estimate_FD = OFDM_channel_estimate(y, pilots, P, N_sc, L_CP, Nt, Nf)    
+channel_estimate_FD = OFDM_channel_estimate(y, pilots, P, N_sc, L_CP, Nt, Nf)
 
 ###############################################################################
 # 5.4 Estimate the data symbols after channel equalization
 ###############################################################################
 
-symbols_hat = np.reshape(OFDM_channel_equalisation(y, channel_estimate_FD),(P*N_sc,))
+symbols_hat = np.reshape(OFDM_channel_equalisation(y, channel_estimate_FD), (P * N_sc,))
 
 ###############################################################################
 # 5.5 Extract the information bits from the detected symbols
 ###############################################################################
 
-bits_hat = inverse_mapping(symbols_hat.reshape((P*N_sc,)), const_type) 
+bits_hat = inverse_mapping(symbols_hat.reshape((P * N_sc,)), const_type)
 
 """
 #6. Radar processing
@@ -251,19 +270,35 @@ bits_hat = inverse_mapping(symbols_hat.reshape((P*N_sc,)), const_type)
 # 6.1 OFDM radar receiver (rx_sig is already synchronised at the start of the
 # preamble)
 ###############################################################################
-delay_doppler_map = SISO_OFDM_DFRC_RADAR_RX(rx_sig, symbols, L_CP, M,zeropad_N,zeropad_P)
+delay_doppler_map = SISO_OFDM_DFRC_RADAR_RX(
+    rx_sig, symbols, L_CP, M, zeropad_N, zeropad_P
+)
 
 """
 # 7. Print and plots
 """
+###############################################################################
+# 7.0 Print the loaded parameters:
+###############################################################################
+print("The loaded parameters from the tx file:")
+
+print("P = " + str(MyDic_param["P"]))
+
+print("B = " + str(MyDic_param["B"]))
+
+print("N_sc = " + str(MyDic_param["N_sc"]))
+
+print("L_CP = " + str(MyDic_param["L_CP"]))
+
+print("L = " + str(MyDic_param["L"]))
 
 ###############################################################################
-# 7.1 Plot the real and imaginary part of the received signal 
+# 7.1 Plot the real and imaginary part of the received signal
 ###############################################################################
 plt.figure()
-plt.subplot(2,1,1)
+plt.subplot(2, 1, 1)
 plt.plot(np.real(rx_sig))
-plt.subplot(2,1,2)
+plt.subplot(2, 1, 2)
 plt.plot(np.imag(rx_sig))
 plt.title("RX signal")
 plt.show()
@@ -271,33 +306,36 @@ plt.show()
 ###############################################################################
 # 7.2 Print the Bit Error Rate (BER)
 ###############################################################################
-# Note: Here the BER is defined with the data and pilot symbol. It should 
+# Note: Here the BER is defined with the data and pilot symbol. It should
 # normally only be computed with the data symbols.
-print("BER: ",  np.mean(abs(bits-bits_hat)))  
+print("BER: ", np.mean(abs(bits - bits_hat)))
 
 ###############################################################################
 # 7.3 Plot the delay-Doppler maps
 ###############################################################################
 
 # Axis definition for delay-Doppler map
-delay_axis = np.arange(Ng*zeropad_N) * dtau
-Doppler_axis = (np.arange(P*zeropad_P) - np.floor(P*zeropad_P/2)) * df_D
+delay_axis = np.arange(Ng * zeropad_N) * dtau
+Doppler_axis = (np.arange(P * zeropad_P) - np.floor(P * zeropad_P / 2)) * df_D
 
 # delay-doppler map
 plt.figure()
-plt.pcolormesh(delay_axis,Doppler_axis,abs(delay_doppler_map),shading='nearest')
+plt.pcolormesh(delay_axis, Doppler_axis, abs(delay_doppler_map), shading="nearest")
 plt.ylabel("Doppler frequency [Hz]")
 plt.title("Delay-Doppler map")
+plt.show()
+
 
 ###############################################################################
 # 7.4 Plot the received constellation before and after equalization
 ###############################################################################
 plt.figure()
-plt.plot(np.real(y), np.imag(y), '.b')
-plt.title('Received constellation (before equalisation)')
+plt.plot(np.real(y), np.imag(y), ".b")
+plt.title("Received constellation (before equalisation)")
+plt.show()
 
 plt.figure()
-plt.plot(np.real(symbols_hat), np.imag(symbols_hat), '.b')
-plt.plot(np.real(const), np.imag(const), '.r')
-plt.title('Received constellation')
-
+plt.plot(np.real(symbols_hat), np.imag(symbols_hat), ".b")
+plt.plot(np.real(const), np.imag(const), ".r")
+plt.title("Received constellation")
+plt.show()
--- src/baseline/usrp_tx.py
+++ src/baseline/usrp_tx.py
@@ -13,15 +13,16 @@ Code from:
     * Martin Willame
 """
 
-
 """
 #0 Packages import
 """
 # The constellations
 from radcomlib.comm_toolbox import BPSK_const, QPSK_const, QAM16_const
+
 # Inverse mapping from symbols to bits
 from radcomlib.comm_toolbox import inverse_mapping
-# OFDM TX waveform 
+
+# OFDM TX waveform
 from radcomlib.ofdm import SISO_OFDM_DFRC_TX
 
 # The usual imports
@@ -32,96 +33,98 @@ import numpy as np
 #1. Subfunction definition
 """
 
-def generate_tx(tx_sig,filename):
+
+def generate_tx(tx_sig, filename):
     """
-    The function creates the <.txt> file for the transmission with the 
+    The function creates the <.txt> file for the transmission with the
     experimental setup. The file must be moved into RamDisk to be transmited as
     defined in the jupyter notebook "section 3: Transmission d'un signal"
-    
+
     Parameters
     ----------
     tx_sig : numpy complex vector
         vector with the I/Q samples to transmits
-    
+
     filename : string <filename.txt>
         name of the file with the I/Q samples to transmit
 
     Returns
     -------
     sig_len : int
-        Number of complex samples to transmit. The value must be encoded in the 
-        execution command for the transmission (see section 3: 
+        Number of complex samples to transmit. The value must be encoded in the
+        execution command for the transmission (see section 3:
         Transmission d'un signal of the jupyter notebook)
 
     """
     # The number of complex symbols
     sig_len = len(tx_sig)
-    
+
     # Interleaving between the real and the imaginary part
-    split_sig = np.zeros((sig_len*2))
+    split_sig = np.zeros((sig_len * 2))
     split_sig[::2] = np.real(tx_sig)
     split_sig[1::2] = np.imag(tx_sig)
-    
+
     # Normalisation for the tranmission of the signal. We ensure here that the
     # norm of the samples to transmit is smaller than 1
-    split_sig = split_sig/np.max(split_sig)*0.7     
+    split_sig = split_sig / np.max(split_sig) * 0.7
 
     # Save the result
-    np.savetxt(filename,split_sig)
+    np.savetxt(filename, split_sig)
 
     return sig_len
 
+
 """
 #2. PARAMETERS for the OFDM transmission
-""" 
+"""
 ###############################################################################
 # 2.1 These parameters define the tranmission and can be changed
 ###############################################################################
 
 # Transmission scenario name
 scenario_name = "Setup_40MHz"
-# Number of transmitted pulses (number of OFDM symbols) 
-P = 256 #default: P=256
+# Number of transmitted pulses (number of OFDM symbols)
+P = 256  # default: P=256
 # Bandwidth [Hz]: The maximum available bandwith is 100 MHz
-B = 40e6 #default: B=40MHz
+B = 40e6  # default: B=40MHz
 # Number of subcarriers
-N_sc = 1024 #default: N_sc=1024
-# Cyclic Prefix (CP) length 
-L_CP = 256 #default: N_sc=256
+N_sc = 1024  # default: N_sc=1024
+# Cyclic Prefix (CP) length
+L_CP = 256  # default: N_sc=256
 # Tx oversampling factor:
-L = 5 #default: L=5
+L = 5  # default: L=5
 
 ###############################################################################
 # 2.2 Checks for potential errors
 ###############################################################################
 
-# The maximum sampling rate of the setup is 200MHz for the transmission. 
-# Therefore the product of the bandwith and the oversampling factor must be 
+# The maximum sampling rate of the setup is 200MHz for the transmission.
+# Therefore the product of the bandwith and the oversampling factor must be
 # smaller or equal to 200 MHz
-assert(L*B<=200e6)
+assert L * B <= 200e6
 # The CP length must be smaller than the number of subcarriers
-assert(L_CP<N_sc)
+assert L_CP < N_sc
 
 ###############################################################################
-# 2.3 These values are directly defined from the above parameters 
+# 2.3 These values are directly defined from the above parameters
 ###############################################################################
 
 # Subcarrier spacing [Hz]
-Df = B/N_sc 
+Df = B / N_sc
 # OFDM symbol duration [s]
-T = 1/Df 
-# Time between two samples in time domain if no oversampling factor [s] 
-Tc = 1/B
+T = 1 / Df
+# Time between two samples in time domain if no oversampling factor [s]
+Tc = 1 / B
 # TX sampling frequency [Hz]
-fs_tx = L/Tc 
+fs_tx = L / Tc
 # Duration of the CP [s]
-T_CP = L_CP*Tc
+T_CP = L_CP * Tc
 # Duration of the full OFDM symbol (with the CP included) [s]
 T_PRI = T + T_CP
 # Number of samples in the useful part of the OFDM symbols
-pulse_width = N_sc*L 
+pulse_width = N_sc * L
 # Number of samples in the CP part of the OFDM symbols
-Ng = L_CP*L 
+Ng = L_CP * L
 
 """
 #3. Parameters for the pilots and data symbols
@@ -131,15 +134,15 @@ Ng = L_CP*L
 ###############################################################################
 
 # Constellation ; either "BPSK", "QPSK", "16QAM"
-const_type = "QPSK" # Default: const_type="QPSK"
-# Pilot spacing in the time domain (defines how often the OFDM symbol will 
+const_type = "QPSK"  # Default: const_type="QPSK"
+# Pilot spacing in the time domain (defines how often the OFDM symbol will
 # countain pilots)
-Nt = 1 # Default: Nt=1
-# Pilot spacing in the frequency domain (defines the spacing in number of 
+Nt = 1  # Default: Nt=1
+# Pilot spacing in the frequency domain (defines the spacing in number of
 # subcarriers  between two pilots subcarriers)
-Nf = 1 # Default: Nf=1
+Nf = 1  # Default: Nf=1
 # For example: P=4, N_sc = 8, Nt = 1 and Nf = 2:
-# The subcarriers 0,2,4,6,7 will have pilots for all OFDM symbols 0,1,2,3. All 
+# The subcarriers 0,2,4,6,7 will have pilots for all OFDM symbols 0,1,2,3. All
 # the other subcarriers from the OFDM symbols are data symbols. Note that
 # subcarrier 7 is included to enable interpolation and avoid extrapolation.
 
@@ -158,31 +161,31 @@ elif const_type == "16QAM":
 
 ## Generation of the pilots grid
 
-# Note: the first and the last subcarrier is always included in the 
-# pilots. Same for the first and the last OFDM symbol. This ensure that we will 
+# Note: the first and the last subcarrier is always included in the
+# pilots. Same for the first and the last OFDM symbol. This ensure that we will
 # not perform any extrapolation.
-pilots_idx_f = np.concatenate((np.arange(0,N_sc-1,Nf),[N_sc-1])) 
-pilots_idx_t = np.concatenate((np.arange(0,P-1,Nt),[P-1]))
+pilots_idx_f = np.concatenate((np.arange(0, N_sc - 1, Nf), [N_sc - 1]))
+pilots_idx_t = np.concatenate((np.arange(0, P - 1, Nt), [P - 1]))
 # Representation in two dimensions of the pilots in frequency and time domain
-pilots_idx_t_mesh,pilots_idx_f_mesh = np.meshgrid(pilots_idx_t,pilots_idx_f)
+pilots_idx_t_mesh, pilots_idx_f_mesh = np.meshgrid(pilots_idx_t, pilots_idx_f)
 
-## Random symbols generation for data and preamble 
+## Random symbols generation for data and preamble
 
-# Matrix of size: Number of OFDM symbols x Number of subcarriers (countains 
+# Matrix of size: Number of OFDM symbols x Number of subcarriers (countains
 # both the pilots and data symbols)
-symbols = np.random.choice(const,(P,N_sc)) 
+symbols = np.random.choice(const, (P, N_sc))
 # Vector of size: Number of subcarriers (countains only the preamble symbols
-# transmitted for the packet detection). In this case the preamble is not 
+# transmitted for the packet detection). In this case the preamble is not
 # designed to have good autocorrelation properties
-preamble = np.random.choice(const,(int(N_sc),))
+preamble = np.random.choice(const, (int(N_sc),))
 
 ## Get the corresponding pilots and bits
 
 # Pilots symbols matrix of size: sizeof(pilots_idx_t) x sizeof(pilots_idx_f)
-pilots = symbols[pilots_idx_t_mesh.T,pilots_idx_f_mesh.T] 
-# Corresponding bits for the data and pilot symbol of size: 
+pilots = symbols[pilots_idx_t_mesh.T, pilots_idx_f_mesh.T]
+# Corresponding bits for the data and pilot symbol of size:
 # Number of OFDM symbols*Number of subcarriers
-bits = inverse_mapping(symbols.reshape((P*N_sc,)), const_type)
+bits = inverse_mapping(symbols.reshape((P * N_sc,)), const_type)
 
 """
 # 5. Generation of the samples of the OFDM signal to transmit
@@ -190,7 +193,7 @@ bits = inverse_mapping(symbols.reshape((P*N_sc,)), const_type)
 ## Generation of the signal
 # If periodic_preamble is turned to True, the preamble should be of size N_sc/2
 # as it will be periodised
-tx_sig = SISO_OFDM_DFRC_TX(symbols, preamble, L_CP,L=L, periodic_preamble=False)
+tx_sig = SISO_OFDM_DFRC_TX(symbols, preamble, L_CP, L=L, periodic_preamble=False)
 
 
 """
@@ -198,38 +201,38 @@ tx_sig = SISO_OFDM_DFRC_TX(symbols, preamble, L_CP,L=L, periodic_preamble=False)
 """
 ## Save everything
 
-# Save the transmission samples in the format for the experimental setup and 
-# get the signal length 
-sig_len = generate_tx(tx_sig,scenario_name+"_"+"tx_sig.txt")
+# Save the transmission samples in the format for the experimental setup and
+# get the signal length
+sig_len = generate_tx(tx_sig, scenario_name + "_" + "tx_sig.txt")
 # Save the transmitted signal the pilot and data symbols
-np.save(scenario_name+"_"+"signal.npy",tx_sig)
+np.save(scenario_name + "_" + "signal.npy", tx_sig)
 # Save the pilot and data symbols
-np.save(scenario_name+"_"+"symbols.npy",symbols)
+np.save(scenario_name + "_" + "symbols.npy", symbols)
 # Save the preamble for the synchronization
-np.save(scenario_name+"_"+"preamble.npy",preamble)
+np.save(scenario_name + "_" + "preamble.npy", preamble)
 # Save the transmission parameters
 MyDic_param = {
     "P": P,
     "B": B,
-    "N_sc":N_sc,
-    "L_CP":L_CP,
+    "N_sc": N_sc,
+    "L_CP": L_CP,
     "L": L,
     "const_type": const_type,
-    "Nt":Nt,
-    "Nf":Nf
-    }
+    "Nt": Nt,
+    "Nf": Nf,
+}
 # Check how to save the Dictionary
-np.save(scenario_name+"_"+"parameters.npy",MyDic_param)
+np.save(scenario_name + "_" + "parameters.npy", MyDic_param)
 
 ## Print the sig_len for the execution in the experimental setup
-print("SIG_LEN: ",sig_len)
-print("2x SIG_LEN: ",2*sig_len)
+print("SIG_LEN: ", sig_len)
+print("2x SIG_LEN: ", 2 * sig_len)
 
 ## Plot the transmitted signal
 plt.figure()
-plt.subplot(2,1,1)
+plt.subplot(2, 1, 1)
 plt.plot(np.real(tx_sig))
-plt.subplot(2,1,2)
+plt.subplot(2, 1, 2)
 plt.plot(np.imag(tx_sig))
 plt.title("TX signal")
 plt.show()
